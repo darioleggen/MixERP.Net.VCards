05c40823f9731cb691427c213d55018f47671cf8
a.cervo <a.cervo@citelgroup.it>
2019-07-18 14:57:10 +0200

    cambiata funzione split

--- src/MixERP.Net.VCards/Helpers/VCardHelper.cs
+++ src/MixERP.Net.VCards/Helpers/VCardHelper.cs
@@ -1,12 +1,51 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;   
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MixERP.Net.VCards.Helpers
 {
     public static class VCardHelper
     {
+        public static int CheckOccurrences(string str1, string pattern)
+        {
+            int count = 0;
+            int a = 0;
+
+            while ((a = str1.IndexOf(pattern, a)) != -1)
+            {
+                a += pattern.Length;
+                count++;
+            }
+            return count;
+        }
         public static string[] SplitCards(string contents)
         {
-            return Regex.Split(contents, "((BEGIN:VCARD)(.*)(END:VCARD))");
+            string buffer = contents;
+            List<string> cards=new List<string>();
+            string card = "";
+            string tagstart = "BEGIN:VCARD";
+            string tagend = "END:VCARD";
+            int n = 0;
+            int i = 0;
+
+
+            n = CheckOccurrences(buffer, tagend);
+
+            for (i = 0; i < n; i++) { 
+                
+                card = buffer.Substring(buffer.IndexOf(tagstart), buffer.IndexOf(tagend) + tagend.Length);
+                cards.Add(card);
+                buffer= buffer.Substring(card.Length,buffer.Length-card.Length);
+            }
+    
+
+            return cards.ToArray();
+
+            //return Regex.Split(contents, "((BEGIN:VCARD)(.+?)(END:VCARD))");
+
+            
         }
     }
+
+    
 }
